e955f93759d9c9f412a18609c6d1aacb1192b1f4
Matvey <Matvey@DESKTOP-FC23PH5>
2022-02-21 17:32:26 +0300

    Добавьте файлы проекта.

---
+++ Console/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "    **         **  " << std::endl
+              << "    * *       * *  " << std::endl
+              << "    *  *     *  *  " << std::endl
+              << "    *   *   *   *  " << std::endl
+              << "    *     *     *  " << std::endl;
+
+    std::getchar(); //ожидание ввода, чтобы прога
+    std::getchar(); //не закрывалась после выполнения
+
+    return 0;
+}
